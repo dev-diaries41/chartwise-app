ce978c38a478d9bd328fbf2607e6bd47e7198297
d41dev <d41dev@proton.me>
2024-07-14 11:34:45 +0100

    logic update: fix request module

--- app/lib/requests/request.ts
+++ app/lib/requests/request.ts
@@ -1,133 +1,164 @@
 import { StorageKeys } from "@/app/constants/app";
-import { AuthErrors, MiscErrors, RequestErrors } from "../../constants/errors";
-import * as Storage from "@/app/lib/storage";
-import { JobReceipt, UserPlan } from "@/app/types";
+import { AuthErrors, MiscErrors, RequestErrors, ToolErrors } from "../../constants/errors";
+import * as Storage from "@/app/lib/storage"
+import {JobReceipt, UserPlan} from "@/app/types"
 
-async function fetchWithAuth(url: string, options: RequestInit = {}) {
-  const headers = getAuthHeaders();
-  const response = await fetch(url, { ...options, headers });
-  
-  const newToken = response.headers.get('Authorization')?.split(' ')[1];
-  if (newToken) Storage.set(StorageKeys.token, newToken);
+export function getAuthHeaders (){
+    const currentToken = Storage.get(StorageKeys.token);
+    if (!currentToken || typeof currentToken !=='string' ) throw new Error(AuthErrors.MISSING_JWT_TOKEN);
+    const headers = new Headers();
+    headers.append('Authorization', `Bearer ${currentToken}`);
+    return headers;
+  };
 
-  if (!response.ok) {
-    const errorResponse = await response.json();
-    throw new Error(errorResponse.message || RequestErrors.REQUEST_ERROR);
-  }
-
-  return response.json();
-}
-
-export function getAuthHeaders() {
-  const currentToken = Storage.get(StorageKeys.token);
-  if (!currentToken || typeof currentToken !== 'string') {
-    throw new Error(AuthErrors.MISSING_JWT_TOKEN);
-  }
-  return new Headers({ 'Authorization': `Bearer ${currentToken}` });
-}
-
-export async function getNewToken({ userId }: { userId: string }): Promise<string> {
-  if (!userId) throw new Error(AuthErrors.INVALID_USER_ID);
-
-  const formData = new FormData();
-  formData.append('userId', userId);
-
-  const { headers } = await fetchWithAuth('/api/auth/token', {
-    method: 'POST',
-    body: formData,
-  });
-
-  const newToken = headers.get('Authorization')?.split(' ')[1];
-  if (!newToken) throw new Error(AuthErrors.MISSING_JWT_TOKEN);
-
-  Storage.set(StorageKeys.token, newToken);
-  return newToken;
-}
-
-export async function getJobStatus(jobId: string): Promise<{ data: { output: string }, status: JobReceipt["status"] }> {
-  const { data, status } = await fetchWithAuth(`/api/analysis/results?jobId=${jobId}`, { method: 'GET' });
-  if (!data) throw new Error(MiscErrors.UNKNOWN_ERROR);
-  return { data, status };
-}
-
-export async function submitAnalysisRequest(formData: FormData): Promise<string> {
-  const { data } = await fetchWithAuth('/api/analysis', {
-    method: 'POST',
-    body: formData,
-  });
-  if (!data?.jobId) throw new Error(MiscErrors.UNKNOWN_ERROR);
-  return data.jobId;
-}
-
-export async function getUsage(userId: string | null | undefined, plan: UserPlan): Promise<number> {
-  if (!userId) return 0;
-  
-  const endpointUrl = plan === 'Free' ? `/api/usage/daily?userId=${userId}` : `/api/usage/monthly?userId=${userId}`;
-  const { data, message } = await fetchWithAuth(endpointUrl, { method: 'GET' });
-  
-  if (!data) throw new Error(message);
-  return data;
-}
-
-export class RetryHandler {
-  private maxRetries: number;
-  private currentRetry: number;
-
-  constructor(maxRetries: number = 3) {
-    this.maxRetries = maxRetries;
-    this.currentRetry = 0;
-  }
-
-  async retry<T>(func: () => Promise<T>, errorHandler: (error: any) => Promise<boolean>): Promise<T> {
+  export async function getNewToken({ userId }: { userId: string}): Promise<string> {
     try {
-      return await func();
-    } catch (error) {
-      if (this.currentRetry < this.maxRetries) {
-        this.currentRetry++;
-        const shouldRetry = await errorHandler(error);
-        if (shouldRetry) {
-          return await this.retry(func, errorHandler);
+      if (!userId) throw new Error(AuthErrors.INVALID_USER_ID);
+  
+      const formData = new FormData();
+      formData.append('userId', userId);
+    
+      const response = await fetch('/api/auth/token', { method: 'POST', body: formData });
+      if (!response.ok) throw new Error(`Failed to fetch token: ${response.statusText}`);
+  
+      const authHeader = response.headers.get('Authorization');
+      const newToken = authHeader?.split(' ')[1];
+      if (!newToken) throw new Error('Failed to get token');
+  
+      Storage.set(StorageKeys.token, newToken);
+      return newToken;
+    } catch (error: any) {
+      console.error('Error in getNewToken:', error.message);
+      throw error
+    }
+  }
+  
+  export async function getJobStatus (jobId: string): Promise<{data: {output: string}, status: JobReceipt["status"]}> {
+    const headers = getAuthHeaders();
+    const endpointUrl = `/api/analysis/results?jobId=${jobId}`;
+    const response = await fetch(endpointUrl, { method: 'GET', headers });
+  
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.message || RequestErrors.POLLING_ERROR);
+    }
+  
+    const newToken = response.headers.get('Authorization')?.split(' ')[1];
+    if (newToken) Storage.set(StorageKeys.token, newToken);
+  
+    const { data, status, message } = await response.json();
+    if (!data) throw new Error(message || RequestErrors.POLLING_ERROR);
+  
+    return { data, status };
+  };
+
+  export async function submitAnalysisRequest(formData: FormData): Promise<string> {
+    const endpointUrl = '/api/analysis';
+    const headers = getAuthHeaders();
+  
+    const response = await fetch(endpointUrl, { method: 'POST', body: formData, headers });
+  
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.message || ToolErrors.CHART_ANALYSIS);
+    }
+  
+    const newToken = response.headers.get('Authorization')?.split(' ')[1];
+    if (newToken) Storage.set(StorageKeys.token, newToken);
+  
+    const { message, data } = await response.json();
+    if (!data?.jobId) throw new Error(message || MiscErrors.UNKNOWN_ERROR);
+  
+    return data.jobId;
+  };
+
+  export async function getUsage (userId: string | null | undefined, plan: UserPlan): Promise<number> {
+    if(!userId)return 0
+    const headers = getAuthHeaders();
+    const endpointUrl = plan === 'Free'? `/api/usage/daily?userId=${userId}` : `/api/usage/monthly?userId=${userId}`;
+    const response = await fetch(endpointUrl, { method: 'GET', headers });
+  
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.message);
+    }
+  
+    const newToken = response.headers.get('Authorization')?.split(' ')[1];
+    if (newToken) Storage.set(StorageKeys.token, newToken);
+  
+    const { data, message } = await response.json();
+    const credits = data;
+    if (!credits) throw new Error(message);
+  
+    return credits ;
+  };
+  
+
+  export class RetryHandler {
+    private maxRetries: number;
+    private currentRetry: number;
+  
+    constructor(maxRetries: number = 3) {
+      this.maxRetries = maxRetries;
+      this.currentRetry = 0;
+    }
+  
+    async retry<T>(func: () => Promise<T>, errorHandler: (error: any) => Promise<boolean>): Promise<T> {
+      try {
+        return await func();
+      } catch (error) {
+        if (this.currentRetry < this.maxRetries) {
+          this.currentRetry++;
+          const shouldRetry = await errorHandler(error);
+          if (shouldRetry) {
+            return await this.retry(func, errorHandler);
+          } else {
+            throw error; // Propagate error if retry conditions are not met
+          }
         } else {
-          throw error;
+          throw error; // Max retries exceeded
         }
-      } else {
-        throw error;
+      } finally {
+        this.reset();
       }
-    } finally {
-      this.reset();
+    }
+  
+    reset() {
+      this.currentRetry = 0;
     }
   }
 
-  reset() {
-    this.currentRetry = 0;
-  }
-}
+  export async function saveAnalysis (analysis: string, chart: string): Promise<string | null> {
+    try {
+      const headers = getAuthHeaders();
+      const formData = new FormData();
+      formData.append('analysis', analysis);
+      formData.append('chart', chart);
+      const response = await fetch('/api/analysis/save', {method: 'POST',body: formData, headers});
+      if (!response.ok) throw new Error('Failed to save analysis');
+      
+      const {data} = await response.json();
+      const id = data;
+      const shareableUrl = `${window.location.origin}/share/${id}`;
+      return shareableUrl;
+    } catch (error) {
+      console.error('Error saving analysis:', error);
+      return null;
+    }
+  };
 
-export async function saveAnalysis(analysis: string, chart: string): Promise<string | null> {
-  try {
-    const formData = new FormData();
-    formData.append('analysis', analysis);
-    formData.append('chart', chart);
-    
-    const { data } = await fetchWithAuth('/api/analysis/save', {
-      method: 'POST',
-      body: formData,
-    });
+  export async function getSharedAnalysis (id: string) {
+    try {
+      const endpointUrl = `/api/analysis/share?id=${id}`;
+      const response = await fetch(endpointUrl, { method: 'GET' });
+      if (!response.ok) throw new Error('Failed to save analysis');
 
-    const shareableUrl = `${window.location.origin}/share/${data}`;
-    return shareableUrl;
-  } catch (error) {
-    console.error('Error in saveAnalysis:', error);
-    return null;
-  }
-}
-
-export async function getSharedAnalysis(id: string) {
-  try {
-    const { analysis, chartUrl } = await fetchWithAuth(`/api/analysis/share?id=${id}`, { method: 'GET' });
-    return { analysis, chartUrl };
-  } catch (error: any) {
-    console.error('Error in getSavedAnalysis:', error.message);
-    throw error;
-  }
-}
+      const data = await response.json();
+      return data as {analysis: string, chartUrl: string};
+    } catch (error: any) {
+      console.error('Error in getSavedAnalysis:', error.message);
+      throw error
+    }
+  };
+  
+  
--- app/stripe.ts
+++ app/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET_LIVE!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_LIVE_KEY!, {
     apiVersion: "2024-04-10",
 });
 
